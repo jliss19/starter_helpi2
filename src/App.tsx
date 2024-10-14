64882a24ef7377702df5e67bd4614ac2ea840fbf
Ronald <ronald@udel.edu>
2024-10-14 13:11:32 -0400

    import 'bootstrap/dist/css/bootstrap.min.css'; omport 'bootstrap/dist/css/bootstrap.min.css';

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, ProgressBar } from 'react-bootstrap';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -13,7 +13,8 @@ if (prevKey !== null) {
 
 function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
-  
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const totalQuestions = 3;
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
@@ -24,13 +25,18 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
+  function nextQuestion() {
+    if (currentQuestion < totalQuestions) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.tsx</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -42,19 +48,41 @@ function App() {
       </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        <Form.Control
+          type="password"
+          placeholder="Insert API Key Here"
+          onChange={changeKey}
+        />
+        <br />
+        <Button className="Submit-Button" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Form>
+
+      {/* Career Assessment Questions */}
       <div>
-        Mason Davis
+        <h2>Career Assessment</h2>
+        {currentQuestion === 0 && <p>Question 1: What is your favorite work environment?</p>}
+        {currentQuestion === 1 && <p>Question 2: What do you enjoy doing in your free time?</p>}
+        {currentQuestion === 2 && <p>Question 3: How do you handle stress?</p>}
+        {currentQuestion < totalQuestions && (
+          <Button onClick={nextQuestion}>Next Question</Button>
+        )}
+
+        {/* Progress Bar */}
+        <ProgressBar
+          now={(currentQuestion / totalQuestions) * 100}
+          label={`${Math.round((currentQuestion / totalQuestions) * 100)}%`}
+          style={{ width: '80%', margin: '20px auto' }}
+        />
       </div>
-      <div> Jonathan Liss </div>
+      
+      <div>Mason Davis</div>
+      <div>Jonathan Liss</div>
       <div>Ronaldo Castillo</div>
       <div>Ronald Kouloun</div>
-      <div> Test </div>
+      <div>Test</div>
     </div>
-
   );
 }
 
