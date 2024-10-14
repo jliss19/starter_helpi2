79149046a3df7db5f42ac001d125d6af216ce52f
Jonathan <jliss@udel.edu>
2024-10-14 13:11:21 -0400

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+export {};
+
+const Footer: React.FC = () => {
+    return (
+        <footer style={styles.footer}>
+            <h1>Footer</h1>
+            <div>
+                <p>Contributors: Jonathan Liss, Ronald Kouloun, Mason Davis, Ronaldo Castillo</p>
+            </div>
+
+        </footer>
+    )
+}
+
+const styles = {
+    footer: {
+        backgroundColor: 'grey',
+        position: 'absolute' as const,
+        bottom: 0,
+        width: '100%',
+        height: '20%',
+    }
+}
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout: React.FC = () => {
     return (
         <div>
-        <Header>
-        </Header>
+        <Footer />
         </div>
     )
 }
