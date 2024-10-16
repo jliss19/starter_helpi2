890d5be42fc7d63d32750585e378ba4eb4468a0a
Jonathan <jliss@udel.edu>
2024-10-16 18:08:32 -0400

    made homepage and navbar better looking, added buttons and descriptions for quizzes

---
+++ src/components/detaileddescription.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function DetailedDescription(): React.JSX.Element {
+    return(
+        <div>
+            Our detailed quiz is a quick and easy seven question quiz that when completed will show a career recommendation based on your answers.
+            The goal of our basic quiz is to allow those who don't have much of an idea of what they want to go into a general idea of what might work for them.
+            THIS IS A PLACEHOLDER!!!
+        </div>
+    )
+}
+
+export default DetailedDescription;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
 import '../styles/Home.css'
+import { BasicQuizDescription } from '../basicquestiondescription';
+import DetailedDescription from '../detaileddescription';
+
+import logo from './logo.svg';
+import {Button} from 'react-bootstrap';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+
 
 const Home: React.FC = () => {
+    const navigate = useNavigate();
     return (
-        <div className = 'Home'>
+        <div>
             <br />
-            <h1 className = 'Home-header'> <u>Take a Career Quiz</u></h1>
+            <h1 className = 'title'><u style = {{padding: '10px'}}>Take a Career Quiz</u></h1>
+            <div style={{ fontStyle: 'italic', padding: '10px' }}>Choose what type of quiz you want to take</div>
+            <div className='grid-container'>
+            <h4 className='left'> Basic Career Questions:</h4>
+            <span className='right'><BasicQuizDescription></BasicQuizDescription></span>
+            </div>
+            <Button className = 'button' onClick={() => navigate('/basic')}>
+            Basic Quiz
+        </Button>            
+
+            <div className='grid-container'>
+            <h4 className='left'> Detailed Career Questions:</h4>
+            <span className='right'><DetailedDescription></DetailedDescription></span>   
+            </div>
+            <Button className = 'button' onClick={() => navigate('/detailed')}>
+            Detailed Quiz
+        </Button>  
+            <div className = 'Home-basicbutton'>
+            
+            </div>
+
         </div>
     )
 }
-
 
 export default Home;
 
