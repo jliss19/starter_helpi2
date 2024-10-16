3ca2be1328d6856eab133577601360525c749946
Jonathan <jliss@udel.edu>
2024-10-16 12:38:07 -0400

    fixed header

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import '../styles/Home.css'
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1> Home Page </h1>
+        <div className = 'Home'>
+            <br />
+            <h1 className = 'Home-header'> <u>Take a Career Quiz</u></h1>
         </div>
     )
 }
 
+
 export default Home;
+
