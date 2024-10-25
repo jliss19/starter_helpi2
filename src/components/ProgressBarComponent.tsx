beb975f10dafc213a3b325dac7d9e98f83f37468
Ronald <ronald@udel.edu>
2024-10-25 13:33:58 -0400

    put export{}

---
+++ src/components/ProgressBarComponent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ProgressBar } from 'react-bootstrap';
+
+interface ProgressBarComponentProps {
+  currentQuestion: number;
+  totalQuestions: number;
+}
+
+const ProgressBarComponent: React.FC<ProgressBarComponentProps> = ({ currentQuestion, totalQuestions }) => {
+  const progress = (currentQuestion / totalQuestions) * 100;
+
+  return (
+    <ProgressBar
+      now={progress}
+      label={`${Math.round(progress)}%`}
+      style={{ width: '80%', margin: '20px auto' }}
+    />
+  );
+};
+
+export default ProgressBarComponent;
+
+export {};
