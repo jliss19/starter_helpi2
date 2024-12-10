eb707069c8b7f74bbfadebc077c9949d3c40aca5
Jonathan <jliss@udel.edu>
2024-12-10 14:58:12 -0500

    fixing linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home3 from './components/pages/Home3';
-import Detailed from './components/pages/Detailed';
-import Basic2 from './components/pages/Basic2';
-import Results from './components/pages/Results';
-import Results2 from './components/pages/Results2';
-import Loading from './components/pages/Loading';
-import About from './components/pages/About';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home3 from "./components/pages/Home3";
+import Detailed from "./components/pages/Detailed";
+import Basic2 from "./components/pages/Basic2";
+import Results2 from "./components/pages/Results2";
+import Loading from "./components/pages/Loading";
+import About from "./components/pages/About";
 
 function App() {
   return (
-    <div className='home-image'>
-      <div className='App'>
+    <div className="home-image">
+      <div className="App">
         <Router>
           <div>
             <main>
